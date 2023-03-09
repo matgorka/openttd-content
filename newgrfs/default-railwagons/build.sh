ac8a7e5c50781aa3f0759eb92a1219db2615d763
Mateusz Górka <119092143+matgorka@users.noreply.github.com>
2023-03-09 02:49:00 +0100

    build.sh now uses build.cfg too

--- newgrfs/default-railwagons/build.sh
+++ newgrfs/default-railwagons/build.sh
@@ -1,9 +1,9 @@
-<default_railwagons.pnml tcc -E - >out.nml
+source ./build.cfg
+<$grf_main $cc $cc_args - >out.nml
 nmlc out.nml
-mkdir default_railwagons 2>/dev/null
-mv out.grf default_railwagons/default_railwagons.grf
-cp *.txt default_railwagons/
-rm default_railwagons/custom_tags.txt 2>/dev/null
-tar -cf 01164d00-default_railwagons.tar default_railwagons
-rm -r default_railwagons
+mkdir $grf_name 2>/dev/null
+mv out.grf $grf_name/$grf_name.grf
+cp changelog.txt license.txt readme.txt $grf_name/ 2>/dev/null
+tar -cf $grf_id-${grf_name}_$grf_version.tar $grf_name
+rm -r $grf_name
 rm out.nml
