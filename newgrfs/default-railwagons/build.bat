b5c17ec9e2bb8cb26567d9032890b05b6c4cec21
Mateusz Górka <119092143+matgorka@users.noreply.github.com>
2023-03-09 02:41:11 +0100

    new build script for Windows

--- newgrfs/default-railwagons/build.bat
+++ newgrfs/default-railwagons/build.bat
@@ -1,10 +1,10 @@
 @echo off
-<default_railwagons.pnml tcc -E - > out.nml
+for /F %%L in (build.cfg) do set %%L
+<%grf_main% %cc% %cc_args% - > out.nml
 nmlc out.nml
-md default_railwagons 2>NUL
-move out.grf default_railwagons\default_railwagons.grf >NUL
-copy *.txt default_railwagons\ >NUL
-del default_railwagons\custom_tags.txt 2>NUL
-tar -cf 01164d00-default_railwagons.tar default_railwagons
-rd /S /Q default_railwagons
+md %grf_name% 2>NUL
+move out.grf %grf_name%\%grf_name%.grf >NUL
+for %%F in (changelog.txt license.txt readme.txt) do copy %%F %grf_name%\ >NUL
+tar -cf %grf_id%-%grf_name%_%grf_version%.tar %grf_name%
+rd /S /Q %grf_name%
 del out.nml
